eba4667dd91cd5abdb0964a0af99fd3c1ee5f132
Artur <artur.borowiec0@interia.pl>
2020-12-01 22:39:48 +0100

    Day1 part2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,21 +2,53 @@ import java.io.File
 
 const val SUM = 2020
 const val FILE = "src/main/resources/input1.txt"
+val CURRENT_TASK = Task.DAY1PART2
 
-fun main(args: Array<String>) {
-    println("TASK: 1  SOLUTION: ${getSolution()}")
+fun main() {
+    printSolution()
 }
 
-fun getSolution(): Int {
+fun printSolution() {
+    println(
+        "${CURRENT_TASK.name} ${when (CURRENT_TASK) {
+            Task.DAY1PART1 -> getSolution11()
+            Task.DAY1PART2 -> getSolution12()
+        }}"
+    )
+}
+
+fun getSolution11(): Int {
+    val data = getData()
+    data.forEach {
+        if (data.contains(SUM - it))
+            return (SUM - it) * it
+    }
+
+    return -1
+}
+
+fun getSolution12(): Int {
+    val data = getData()
+    data.forEachIndexed { i1, el1 ->
+        data.forEachIndexed { i2, el2 ->
+            if (i2 > i1) {
+                if (data.contains(SUM - (el1 + el2)))
+                    return el1 * el2 * (SUM-el1-el2)
+            }
+        }
+    }
+
+    return -1
+}
+
+private fun getData(): List<Int> {
     val data = mutableListOf<Int>()
     File(FILE).forEachLine {
         data.add(it.toInt())
     }
+    return data
+}
 
-    data.forEach {
-        if (data.contains(SUM - it))
-            return (SUM-it) * it
-    }
-
-    return -1
+enum class Task {
+    DAY1PART1, DAY1PART2
 }
