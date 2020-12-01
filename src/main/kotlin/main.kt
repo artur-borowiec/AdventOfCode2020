dd71a77d3c542a0c4872e5a09efdb3a6e416254d
Artur <artur.borowiec0@interia.pl>
2020-12-01 22:00:58 +0100

    Day1 part1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,22 @@
+import java.io.File
+
+const val SUM = 2020
+const val FILE = "src/main/resources/input1.txt"
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("TASK: 1  SOLUTION: ${getSolution()}")
+}
+
+fun getSolution(): Int {
+    val data = mutableListOf<Int>()
+    File(FILE).forEachLine {
+        data.add(it.toInt())
+    }
+
+    data.forEach {
+        if (data.contains(SUM - it))
+            return (SUM-it) * it
+    }
+
+    return -1
 }
